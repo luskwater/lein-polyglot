79e925b4021fbef2aa4ac423361d7969c208e584
Ron Lusk <rlusk@chariotsolutions.com>
2017-12-20 12:02:11 -0500

    Added URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject polyglot/lein-template "0.1.0"
   :description "Generate a Clojure/Java project"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/luskwater/lein-polyglot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
