a16b272544817cf4113192885839f61db8c25c2a
Ron Lusk <rlusk@chariotsolutions.com>
2017-12-20 12:00:44 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject polyglot/lein-template "0.1.0-SNAPSHOT"
+(defproject polyglot/lein-template "0.1.0"
   :description "Generate a Clojure/Java project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
